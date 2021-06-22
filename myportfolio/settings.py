253a18eb9f42a117f8540e561be55d413e738f23
ItsNaSa <naphadesahil@gmail.com>
2021-06-22 23:41:41 +0530

    Debug to false

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'los4oseej33gt3050tzi+-6m^mi+^v5^(el5fpwvr84o%1qm#3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
