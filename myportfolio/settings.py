e86a2354b5ad08a8cab6f5957af497971aa75733
ItsNaSa <naphadesahil@gmail.com>
2021-07-06 00:11:54 +0530

    Added hello wave1

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -123,11 +123,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
